fd45e42298d51ef4324d9394babd55f04f53f3ce
김영일 <rladuddlf3@naver.com>
2020-08-23 23:00:04 +0900

    staking_contract_test1

--- Klaymore.sol
+++ Klaymore.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.5.0;
 
-import "caver-js/packages/caver-kct/src/contract/token/KIP7/KIP7.sol";
-import "caver-js/packages/caver-kct/src/contract/token/KIP7/KIP7Metadata.sol"
-import "caver-js/packages/caver-kct/src/contract/token/KIP7/KIP7Pausable.sol"
+import "./KIP7.sol";
+import "./KIP7Metadata.sol";
+import "./KIP7Pausable.sol";
 
 contract Klaymore is KIP7,KIP7Metadata,KIP7Pausable {
     address private _owner;
@@ -14,13 +14,13 @@ contract Klaymore is KIP7,KIP7Metadata,KIP7Pausable {
     
     modifier onlyOwner(){
     require(msg.sender == _owner);
-    _;
+     _;
     }
     
-    constructor(string memory name, string memory symbol, uint8 decimals) KIP7Metadata(name, symbol, decimals) public { 
-    
+    constructor(string memory name, string memory symbol, uint8 decimals,uint256 amount) KIP7Metadata(name, symbol, decimals) public { 
+    _mint(msg.sender,amount);
     }
-    // 테스트용 클레이예치 및 토큰 반환하는기능.  비율은 예치한 클레이와 동일하게함.
+    
     function createStake(uint256 _stake) public {
         _burn(msg.sender, _stake);
         if(stakes[msg.sender] == 0) addStakeholder(msg.sender);
@@ -44,18 +44,12 @@ contract Klaymore is KIP7,KIP7Metadata,KIP7Pausable {
     }
     return _totalStakes;
     }
-    //스테이킹한 사람을 조회하는 기능
+
     function isStakeholder(address _address)public view returns(bool, uint256) {
     for (uint256 s = 0; s < stakeholders.length; s += 1){
         if (_address == stakeholders[s]) return (true, s);
     }
     return (false, 0);
-    }
-    // 개인과 개인끼리 토큰 거래를 할수있는 함수 단순하게 
-    //호출자(msg.sender)가 to(recipient) 에게 토큰을 얼마만큼(amount)를 보내는 함수 
-    function transfer(address recipient, uint256 amount) public returns (bool) {
-        _transfer(msg.sender, recipient, amount);
-        return true;
     }
     
     function addStakeholder(address _stakeholder) public {
@@ -72,19 +66,11 @@ contract Klaymore is KIP7,KIP7Metadata,KIP7Pausable {
     }
     // ---------- REWARDS ----------
     
-    /**
-     * @notice A method to allow a stakeholder to check his rewards.
-     * @param _stakeholder The stakeholder to check rewards for.
-     */
     function rewardOf(address _stakeholder) public view returns(uint256)
     {
         return rewards[_stakeholder];
     }
 
-    /**
-     * @notice A method to the aggregated rewards from all stakeholders.
-     * @return uint256 The aggregated rewards from all stakeholders.
-     */
     function totalRewards() public view returns(uint256) {
         uint256 _totalRewards = 0;
         for (uint256 s = 0; s < stakeholders.length; s += 1){
@@ -93,17 +79,10 @@ contract Klaymore is KIP7,KIP7Metadata,KIP7Pausable {
         return _totalRewards;
     }
 
-    /** 
-     * @notice A simple method that calculates the rewards for each stakeholder.
-     * @param _stakeholder The stakeholder to calculate rewards for.
-     */
     function calculateReward(address _stakeholder) public view returns(uint256) {
         return stakes[_stakeholder] / 100;
     }
 
-    /**
-     * @notice A method to distribute rewards to all stakeholders.
-     */
     function distributeRewards() public onlyOwner {
         for (uint256 s = 0; s < stakeholders.length; s += 1){
             address stakeholder = stakeholders[s];
@@ -112,9 +91,6 @@ contract Klaymore is KIP7,KIP7Metadata,KIP7Pausable {
         }
     }
 
-    /**
-     * @notice A method to allow a stakeholder to withdraw his rewards.
-     */
     function withdrawReward() public {
         uint256 reward = rewards[msg.sender];
         rewards[msg.sender] = 0;
